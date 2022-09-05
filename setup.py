c0a98adeab22839c3292cbd3f6628201330bcfa0
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-09-05 14:12:04 +0800

    added setup script

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from polyFit import version
+import os
+import json
+
+data = [version.git_origin, version.git_hash,
+		version.git_description, version.git_branch]
+with open(os.path.join('polyFit', 'GIT_INFO'), 'w') as outfile:
+	json.dump(data, outfile)
+
+
+def package_files(package_dir, subdirectory):
+	# walk the input package_dir/subdirectory
+	# return a package_data list
+	paths = []
+	directory = os.path.join(package_dir, subdirectory)
+	for (path, directories, filenames) in os.walk(directory):
+		for filename in filenames:
+			path = path.replace(package_dir + '/', '')
+			paths.append(os.path.join(path, filename))
+	return paths
+
+
+data_files = package_files('PolyFit', 'data')
+
+setup_args = {
+	'name':         'polyFit',
+	'author':       'Chuneeta Nunhokee',
+	'url':          'https://github.com/Chuneeta/polyFit',
+	'license':      'BSD',
+	'version':      version.version,
+	'description':  'MWA Calibration Polynomial Fitting',
+	'packages':     ['PolyFit'],
+	'package_dir':  {'PolyFit': 'PolyFit'},
+	'package_data': {'PolyFit': data_files},
+	'install_requires': ['numpy>=1.16.5','matplotlib>=2.2', 'pytest'],
+	'include_package_data': True,
+	'zip_safe':     False,
+}
+
+if __name__ == '__main__':
+	setup(*(), **setup_args)
