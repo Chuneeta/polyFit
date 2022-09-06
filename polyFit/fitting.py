1717b485a9eb337782f3d7dc79265ac24b6e64ec
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-09-06 17:05:39 +0800

    added HDUTable with fitting coefficients

--- polyFit/fitting.py
+++ polyFit/fitting.py
@@ -4,6 +4,7 @@ import numpy as np
 
 # Number of coarse bands
 Nband = 24
+polstrs = ['XX', 'XY', 'YX', 'YY']
 
 class Fit(object):
 	def __init__(self, calfits):
@@ -48,6 +49,10 @@ class Fit(object):
 	def fit_data(self, order, weights=None, edge_width=2, clip_width=0):
 		self.poly_array = np.zeros_like(self.gain_array, dtype=complex)
 		unflagged_chs = self.unflagged_channels(edge_width, clip_width)
+		cols = []
+		times , antennas = [], []
+		polarizations, complx = [], []
+		coeffs = []
 		for t in range(self.Ntimes):
 			# filter any channels where result is NaN
 			good_chs = [ch for ch in unflagged_chs if not np.isnan(self.convergence[t, ch])]
@@ -58,6 +63,23 @@ class Fit(object):
 					fit_real, fit_imag = self._polynomial_fit(
 						good_chs, self.gain_array[t, ant, good_chs, pol], order=order, weights=weights)
 					self.poly_array[t, ant, :, pol] = fit_real(np.arange(self.Nfreq)) + fit_imag(np.arange(self.Nfreq)) * 1j
+					# writing to fits colums
+					for i, cp in enumerate(['REAL', 'IMAG']):
+						times.append(t)
+						antennas.append(ant[0])
+						polarizations.append(polstrs[pol])
+						complx.append(cp)
+						coeffs.append(fit_real.coef.tolist()) if i % 2 == 0 else coeffs.append(fit_imag.coef.tolist())
+
+		coeffs = np.array(coeffs)
+		_sh = coeffs.shape
+		cols.append(fits.Column(name = 'Timestamp', format='I', array=np.array(times)))
+		cols.append(fits.Column(name = 'Antenna', format='I', array=np.array(antennas)))
+		cols.append(fits.Column(name = 'Pol', format='2A', array=np.array(polarizations)))
+		cols.append(fits.Column(name = 'Complex', format='4A', array=np.array(complx)))
+		for cf in range(_sh[1]):
+			cols.append(fits.Column(name = 'Coeff{}'.format(cf), format='E', array=coeffs[:, cf]))
+		self.poly_params = fits.BinTableHDU.from_columns(cols, name='FIT_COEFFS')
 
 	def mean_square_error(self):
 		try:
@@ -68,13 +90,16 @@ class Fit(object):
 			print ('Fit Object has no fitting attribute. Fitting of the data is required.')
 			return None
 
-	def write_to(self, outfile=None, overwrite=True):
+	def write_to(self, outfile=None, overwrite=True, **kwargs):
 		if outfile is None:
 			outfile = self.calfits.replace('.fits', '_poly.fits')
 		try:
 			with fits.open(self.calfits) as hdus:
 				hdus['SOLUTIONS'].data[:, :, :, ::2] = np.real(self.poly_array)
 				hdus['SOLUTIONS'].data[:, :, :, 1::2] = np.imag(self.poly_array)
+				hdus.append(self.poly_params)
+				for key, value in kwargs.items():
+					hdus['FIT_COEFFS'].header.insert('TTYPE1', (key, value))
 				hdus.writeto(outfile, overwrite=overwrite)
 		except AttributeError:
 			print ('Fit Object has no fitting attribute. Fitting of the data is required. No file is being written.')
